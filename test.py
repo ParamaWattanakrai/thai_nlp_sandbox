c9fe1bfeb0a1d0b095ea6f8e6171f9678186fa5d
PNPengy <Parama.Wattanakrai@gmail.com>
2023-04-14 23:01:21 +0700

    Function Extraction Pioneering

--- test.py
+++ test.py
@@ -1,17 +1,74 @@
-import re
+from dict import *
 
-string = "This string contains nxains"
+text = ('เคราะห์')
+no_tones = []
+text_lists = []
+text_vowels = []
+split_indexes = []
+syllables = []
 
-# Use a regular expression pattern that matches 'nx', followed by any character, followed by 'ains'
-pattern = re.compile("n.{1}ains")
+# FIRST LAST CHECKER
+# ะ  ์ function
+# index out of range fix
 
-# Search for the pattern within the string
-match = pattern.search(string)
+for true_index, char in enumerate(text):
+    text_lists.append([char, true_index])
+    no_tones.append([char, true_index])
+    if char in th_chars['vowels']:
+        text_vowels.append([char, true_index])
+    if char in th_chars['diacritics']['tones']:
+        no_tones.pop()
 
-# Check if the pattern was found
-if match:
-    # If the pattern was found, print out the actual substring that was matched
-    substring = match.group()
-    print("Found", substring, "in string")
-else:
-    print("Did not find 'nxains' in string")
+def no_tones_index(vowels):
+    #Find no_tones index
+            for index, sublists in enumerate(no_tones):
+                if sublists[1] == vowels[1]:
+                    return index
+
+def check_true_blend(current):
+    print(no_tones[current+2][0])
+    if no_tones[current+2][0] in th_chars['consonants']['blends']:
+        if no_tones[current+1][0] in th_utils['blend_initials'][no_tones[current+2][0]]:
+            return True
+
+def check_poly_follow(current):
+    if no_tones[current+3][0] in th_utils['poly_follows'][no_tones[current][0]]:
+         return True
+
+def poly_initial(vowels):
+    if vowels[0] in th_utils['poly_initials']:
+        current = no_tones_index(vowels)
+        if check_true_blend(current) == True:
+            if check_poly_follow(current) == True:
+                if no_tones[current+3][0] == 'ะ':
+                    if no_tones[current+5][0] == '์':
+                        #INFRONT WIP
+                        split_indexes.append(no_tones[current+5][1])
+                    else:
+                        #INFRONT WIP
+                        split_indexes.append(no_tones[current+3][1])
+                elif no_tones[current+3][0] == 'า':
+                    if no_tones[current+4][0] == 'ะ':
+                        if no_tones[current+6][0] == '์':
+                            #INFRONT WIP
+                            split_indexes.append(no_tones[current+6][1])
+                        else:
+                            #INFRONT WIP
+                            split_indexes.append(no_tones[current+4][1])
+                    else:
+                        if no_tones[current+5][0] == '์':
+                            #INFRONT WIP
+                            split_indexes.append(no_tones[current+5][1])
+                        else:
+                            #INFRONT WIP
+                            split_indexes.append(no_tones[current+3][1])
+                            print('yes')
+
+
+for vowels in text_vowels:
+    #Find poly_initial
+    poly_initial(vowels)
+
+
+
+print(split_indexes)
